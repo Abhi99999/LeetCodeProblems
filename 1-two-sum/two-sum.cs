0b03add5809a4b4815afc2d266d5dad0f82ce5e9
Abhi99999 <67671261+Abhi99999@users.noreply.github.com>
2026-01-04 18:30:26 +0530

    Time: 1 ms (99.10%) | Memory: 49.2 MB (22.63%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        // 1st int is difference and 2nd int is index
+        Dictionary<int,int> map = new Dictionary<int,int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            int diff = target - num;
+            if(map.ContainsKey(diff))
+            return new int[] {i, map[diff]};
+            else if(!map.ContainsKey(num))
+            map.Add(num,i);
+        }
+         return Array.Empty<int>();
+    }
+}
