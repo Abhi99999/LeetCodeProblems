24b537cb4810414e91c6c2d9b871ac424104fb40
Abhi99999 <67671261+Abhi99999@users.noreply.github.com>
2026-01-09 10:43:55 +0530

    Time: 25 ms (59.18%) | Memory: 28.8 MB (93.18%) - LeetSync

--- 7-reverse-integer/reverse-integer.cs
+++ 7-reverse-integer/reverse-integer.cs
@@ -5,9 +5,9 @@ public class Solution {
         while(x != 0)
         {
             num = x%10;
-            if(value > int.MaxValue/10 || (value == int.MaxValue/10 && num == 7))
+            if(value > int.MaxValue/10 || (value == int.MaxValue/10 && num > 7))
             return 0;
-            if(value < int.MinValue/10 ||(value == int.MinValue/10 && num == -8))
+            if(value < int.MinValue/10 ||(value == int.MinValue/10 && num < -8))
             return 0;
             value = value*10 + num;
             x /= 10;
