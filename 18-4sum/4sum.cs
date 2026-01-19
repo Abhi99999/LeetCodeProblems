18a33e9317c66c531f9ed00e88917f13cfef385b
Abhi99999 <67671261+Abhi99999@users.noreply.github.com>
2026-01-19 18:21:27 +0530

    Time: 14 ms (74.16%) | Memory: 48.6 MB (87.36%) - LeetSync

---
+++ 18-4sum/4sum.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public IList<IList<int>> FourSum(int[] nums, int target) {
+        IList<IList<int>> result = new List<IList<int>>();
+        Array.Sort(nums);
+        int n = nums.Length;
+        if(n<4)
+            return result;
+        if (n == 4)
+        {
+            if ((long)nums[0] + nums[1] + nums[2] + nums[3] == target)
+                result.Add(new List<int> { nums[0], nums[1], nums[2], nums[3] });
+            return result;
+        }
+        for(int i = 0; i<n-3; i++)
+        {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            for(int j=i+1; j<n-2; j++)
+            {
+                if (j>i+1 && nums[j] == nums[j-1])
+                    continue;
+                int left = j+1;
+                int right = n-1;
+                while(left<right)
+                {
+                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum == target)
+                    {
+                        result.Add(new List<int> {nums[i], nums[j], nums[left], nums[right]});
+                        while(left<right && nums[left]==nums[left+1])
+                            left++;
+                        while(left<right && nums[right]==nums[right-1])
+                            right--;
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target)
+                        left++;
+                    else
+                        right--;
+                }
+            }
+        }
+        return result;
+    }
+}
